440de3c1ad20926c42cfd04da23ce5c61a0d7d9b
Clément.A <125081819+ITClmt@users.noreply.github.com>
2025-01-31 23:31:55 +0100

    fixed deployment 04

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,9 @@
-import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
   server: {
     port: 3000,
     proxy: {
